6bc93712d1f1ed7f510156a1ad64de2e99c80aef
Ryan Tucker <rtucker88@gmail.com>
2014-04-15 22:10:42 -0400

    Inter-controller service communication

--- angular101.js
+++ angular101.js
@@ -1,1 +1,17 @@
 'use strict';
+
+var app = angular.module('myFirstApp', []);
+
+app.controller('firstCtrl', function ($scope, CommService) {
+    $scope.shared = CommService;
+});
+
+app.controller('secondCtrl', function ($scope, CommService) {
+    $scope.shared2 = CommService;
+});
+
+app.service('CommService', function () {
+    this.hello = 'hi from comm service';
+
+    return this;
+});
